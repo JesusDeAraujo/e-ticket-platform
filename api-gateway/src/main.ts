167b930118d8c501c2f0ba85239d575c0fd19b68
Jesús De Araujo <jesus5dearaujo@gmail.com>
2026-05-04 10:14:30 -0400

    Seguridad
    Configuradas las reglas de seguridad como rateLimit, helmet y Cors

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+/* async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
+ */
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  
+  app.use(helmet());
+  app.enableCors();
+
+  
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Gateway corriendo en puerto: ${port}`);
 }
 bootstrap();
