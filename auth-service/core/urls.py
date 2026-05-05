6f88a0cab2b3f1ef8318b1318149a37464188660
Jesús De Araujo <jesus5dearaujo@gmail.com>
2026-05-04 23:36:45 -0400

    JWT
    Emisión de Access Token y Refresh Token

--- auth-service/core/urls.py
+++ auth-service/core/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import hello_world
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world)
+    path('hello/', hello_world),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
