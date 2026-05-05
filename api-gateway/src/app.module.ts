4d0fcfce50bc33b5131ced4013e122ac72b5ab30
Jesús De Araujo <jesus5dearaujo@gmail.com>
2026-05-04 20:49:41 -0400

    API Gateway
    Configuración del API Gateway, junto con el Cors y Rate Limiting completos

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({
+/* @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
+})
+export class AppModule {} */
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { ConfigService } from '@nestjs/config';
 
 /* async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,20 +14,31 @@ bootstrap();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
-  
   app.use(helmet());
   app.enableCors();
 
-  
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000,
       max: 100,
+      message: 'Limite de peticiones superado, intenta nuevamente mas tarde',
     }),
   );
 
-  const port = process.env.PORT || 3000;
+  const authServiceUrl = configService.get<string>('AUTH_SERVICE_URL');
+
+  app.use(
+    '/auth',
+    createProxyMiddleware({
+      target: authServiceUrl,
+      changeOrigin: true,
+      pathRewrite: { '^/auth': '' },
+    }),
+  );
+
+  const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
   console.log(`Gateway corriendo en puerto: ${port}`);
 }
