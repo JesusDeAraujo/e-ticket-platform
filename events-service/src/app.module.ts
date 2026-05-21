2ae3f0e46bc09d0f3af5abe3e0146b523683114f
Jesús De Araujo <jesus5dearaujo@gmail.com>
2026-05-20 20:01:06 -0400

    Corrección Events-service
    Inicializado el uso de variables de entorno en el servicio de eventos

--- events-service/src/app.module.ts
+++ events-service/src/app.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/events_db'), //Por los momentos usaremos localhost
-    EventsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/events_db'),
+    EventsModule
   ],
   controllers: [AppController],
   providers: [AppService],
